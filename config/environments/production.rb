2cd2d2c6cee88c073962e4324098bf0737352d45
rshakhb10 <rsfbb05@gmail.com>
2015-11-20 18:41:54 -0500

    fixing mail delivery host.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'culturo-spect-app.herokuapp.com'}
+  config.action_mailer.perform_deliveries = true
+
 end
