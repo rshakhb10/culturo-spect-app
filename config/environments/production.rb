e2ecb21ddc7d72df4da6e5cf39249320adfa129c
rshakhb10 <rsfbb05@gmail.com>
2015-11-20 18:54:20 -0500

    fixing the mailer issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'culturo-spect-app.herokuapp.com'}
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
 
 end
